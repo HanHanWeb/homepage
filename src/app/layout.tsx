34cc43773b71f3a26a172d5c04f2e4ce52450741
Han <1956526909@qq.com>
2026-09-19 16:55:58 +0800

    feat: 接入 51LA 网站统计（异步安装，afterInteractive 不阻塞首屏）

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -66,6 +67,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             {children}
           </LanguageProvider>
         </ThemeProvider>
+        {/* 51LA 网站统计（异步安装，不阻塞首屏渲染） */}
+        <Script id="la-collect" strategy="afterInteractive">{`!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"LK9OFthiXdU6n1CZ",ck:"LK9OFthiXdU6n1CZ",autoTrack:true,hashMode:true});`}</Script>
       </body>
     </html>
   );
